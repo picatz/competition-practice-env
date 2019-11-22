c0eb5eb1633b6b9c20f2b76afbdb033f130eb0c9
Kent 'picat' Gruber <kgruber1@emich.edu>
2019-11-21 21:32:15 -0500

    Update template.tf

--- scoring_engine/template.tf
+++ scoring_engine/template.tf
@@ -1,8 +1,27 @@
-resource "null_resource" "build_go_app" {
+
+data "local_file" "backend_app_source" {
+  filename = "app/front-end/index.html"
+}
+
+data "local_file" "frontend_app_source" {
+  filename = "app/main.go"
+}
+
+resource "null_resource" "check_packr_installed" {
+  provisioner "local-exec" {
+    command = "command -v packr"
+  }
+
+  depends_on = [data.local_file.frontend_app_source, data.local_file.backend_app_source]
+}
+
+resource "null_resource" "build_go_app_with_packr" {
   provisioner "local-exec" {
     working_dir = "app"
     command = "GOOS=linux GOARCH=amd64 packr build -o scoring-engine ."
   }
+
+  depends_on = [null_resource.check_packr_installed]
 }
 
 resource "null_resource" "template" {
@@ -11,5 +30,5 @@ resource "null_resource" "template" {
     command = "bash build.sh ${var.project}"
   }
 
-  depends_on = [null_resource.build_go_app]
+  depends_on = [null_resource.build_go_app_with_packr]
 }
